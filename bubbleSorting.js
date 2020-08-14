04817c6537cf376487fdcc7e33c0a20a43459561
velveet <velveet@pm.me>
2020-08-14 14:38:28 -0500

    neat code. fixing mess ups.

--- bubbleSorting.js
+++ bubbleSorting.js
@@ -1,5 +1,5 @@
 let auxPosition;
-let arr1 = [9, 5, -2, 6];
+let arr1 = [9,5,-2,6,20,15,17];
 
 console.log(arr1);
 console.log('-------------------');
@@ -8,9 +8,9 @@ for (let i = 0; i < arr1.length; i++) {
     for (let j = 0; j < arr1.length - i - 1; j++) {
         if(arr1[j] > arr1[j+1]) {
             console.log("swapping " + arr1[j] + " & " + arr1[j+1])
-            auxPosition = arr1[j];
-            arr1[j] = arr1[j+1];
-            arr1[j+1] = auxPosition;
+            auxPosition = arr1[j]; /* saving the j (current) position on the auxPosition variable */
+            arr1[j] = arr1[j+1]; /* updating the jth position with a lower value */
+            arr1[j+1] = auxPosition; /* updating the jth + 1 with a greater value */
         }     
     }
 }
--- insertionSort.js
+++ insertionSort.js
@@ -1,13 +1,13 @@
 let minValue;
-let arr1 = [9,1,3,2,5,20,6,7];
+let arr1 = [9,5,-2,6,20,15,17];
 console.log(arr1);
 
 for (let i = 0; i < arr1.length; i++) {
     minValue = arr1[i];
     for (let j = i+1; j < arr1.length; j++) {
         if (minValue>arr1[j]) {
-            arr1[i] = arr1[j]; /* swap values, means there's a value lower than the minimum */   
-            arr1[j] = minValue; /* updating the Nth value */
+            arr1[i] = arr1[j]; /* swap values, means there's a value lower than the (current) minimum */   
+            arr1[j] = minValue; /* updating the jth value */
             minValue = arr1[i]; /* update the min value */
         }
     }
